6550f15a3cc69fc216fe5237e04dd50e72d1694f
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-12-31 22:31:00 +0400

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <div id="app">
-    <h1>New Year 2024 Countdown</h1>
+  <!-- Special Card for Closest Timezone -->
+  <div v-if="closestCity" :class="['next', (closestCity.timeLeft.totalMilliseconds > 15 * 60 * 1000) ? 'soon' : 'very-soon']">
+    <p class="next-caption">New Year 2024</p>
+    <p class="next-cities">{{ closestCity.name }}</p>
+    <p class="next-timezones">{{ closestCity.timezone }}</p>
+    <p class="next-time-left">
+      {{ formatTimeLeftString(closestCity.timeLeft) }}
+    </p>
+</div>
 
-    <!-- Special Card for Closest Timezone -->
-    <div v-if="closestCity" class="closest-city">
-      <h2>Up Next: {{ closestCity.name }} ({{ closestCity.timezone }})</h2>
-      <p>
-        {{ formatTimeLeftString(closestCity.timeLeft) }}
-      </p>
-    </div>
-
+  <div class="upcoming">
     <!-- List of Other Timezones -->
-    <div v-for="(city, index) in otherCities" :key="index" class="city">
-      {{ city.name }} ({{ city.timezone }})
-      {{ formatTimeLeftString(city.timeLeft) }}
+    <div v-for="(city, index) in otherCities" :key="index" class="upcoming-item">
+      <p class="upcoming-item-name">{{ city.name }}</p>
+      <p class="upcoming-item-timezone">({{ city.timezone }})</p>
+      <p class="upcoming-item-time-left">{{ formatTimeLeftString(city.timeLeft) }}</p>
     </div>
   </div>
 </template>
@@ -83,16 +84,124 @@ export default {
 </script>
 
 <style>
-/* Add styles as needed */
-.closest-city {
-  background-color: lightgreen; /* Highlight the closest city */
-  padding: 10px;
-  margin-bottom: 20px;
+/* Общие стили */
+#app {
+  font-family: 'Arial', sans-serif;
+  display: flex;
+  flex-direction: column;
+  margin: 2vh auto;
+  padding: 2vh 1vw;
 }
 
-.city {
-  margin: 10px 0;
-  padding: 10px;
-  border: 1px solid #ccc;
+/* Стили для специальной карточки ближайшего часового пояса */
+.next {
+  border: 1px solid #dee2e6;
+  border-radius: 1vh;
+  padding: 2vh 1vw;
+  margin-bottom: 2vh;
+  text-align: center;
 }
+
+.next.soon {
+  background-color: #28a745 ; /* Желтый цвет для ближайшего часового пояса */
+}
+
+.next.very-soon {
+  background-color: #cc2535; /* Красный цвет для ближайшего часового пояса */
+}
+
+.next-caption {
+  font-size: 4vh; /* Увеличенный размер для заголовка */
+  color: #007bff; /* Синий цвет для заголовка */
+  margin-bottom: 1vh;
+}
+
+.next-cities,
+.next-timezones {
+  font-size: 3vh; /* Шрифт для городов и часовых поясов */
+  color: #495057; /* Темно-серый цвет текста */
+  margin-bottom: 0.5vh;
+}
+
+.next-time-left {
+  font-size: 3.5vh; /* Шрифт для обратного отсчёта */
+  color: #fff; /* Зеленый цвет */
+  font-weight: bold; /* Жирный шрифт для акцента */
+  margin-bottom: 1vh;
+}
+
+/* Стили для списка остальных часовых поясов */
+.upcoming {
+  display: flex;
+  flex-direction: column;
+}
+
+.upcoming-item {
+  background-color: #fff;
+  border: 1px solid #dee2e6;
+  border-radius: 1vh;
+  padding: 1.5vh 1vw;
+  margin: 1vh 0;
+  font-size: 2vh;
+  color: #212529;
+}
+
+.upcoming-item-name,
+.upcoming-item-timezone,
+.upcoming-item-time-left {
+  margin-bottom: 0.5vh;
+}
+
+/* Адаптивные стили для экранов шире 1024px */
+@media (min-width: 1024px) {
+  #app {
+    flex-direction: row;
+    max-height: 100vh;
+    padding: 0;
+    margin: 0;
+  }
+
+  div, p {
+    margin: 0;
+    padding: 0;
+  }
+
+  .next {
+    flex: 0 0 75%; 
+  }
+
+  .upcoming {
+    flex: 0 0 25%; 
+    overflow-x: hidden;
+    overflow-y: scroll
+  }
+  
+  .next-cities {
+    font-size: 12vh;
+    color: #fff;
+    margin-bottom: 0.5vh;
+    line-height: 30vh;
+  }
+
+  .next-caption {
+    font-size: 6vh;
+    color: #fff;
+    margin-bottom: 1vh;
+    line-height: 20vh;
+  }
+
+  .next-timezones {
+    color: #eeeb;
+    line-height: 20vh;
+    font-size: 1.5vw;
+    line-height: 15vh;
+  }
+
+  .next-time-left {
+    line-height: 30vh;
+    font-size: 20vh;
+  }
+}
+
+
 </style>
